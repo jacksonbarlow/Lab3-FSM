458f67c6be39d7d294e16ee0e982a2b2b9c3f5a3
School <jackson@Jackson-Laptop.localdomain>
2022-11-08 17:45:53 +0000

    Task 1 step 1 working from slide 17 lecture 4

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -1,28 +1,15 @@
-module lsfr #(
-    parameter DATA_WIDTH = 4
-)(
+module lsfr (
     input logic         rst,
     input logic         en,
     input logic         clk,
     input logic         dout
 );
 
-    logic [4:1]         sreg;
+always_ff @ (posedge clk, posedge rst)
+    if (rst)
+        sreg <= 4'b1;
+    else if (en)
+        sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
 
-    always_ff @ (posedge clk)
-        if (rst)
-            sreg <= 4'b0001;
-        else if (en) begin
-            sreg[4] <= sreg[3];
-            sreg[3] <= sreg[2];
-            sreg[2] <= sreg[1];
-            sreg[1] <= sreg[3] xor sreg[4];
-        end
-    begin
-        assign dout[3] <= sreg[4];
-        assign dout[2] <= sreg[3];
-        assign dout[1] <= sreg[2];
-        assign dout[0] <= sreg[1];
-    end
-
+assign dout = sreg;
 endmodule
