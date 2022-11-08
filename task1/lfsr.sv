2944ec5d57496845a43d317bc2e819359c1f66b7
School <jackson@Jackson-Laptop.localdomain>
2022-11-08 18:30:13 +0000

    task1 step2 complete

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -1,9 +1,11 @@
-module lsfr (
+module lfsr #()(
     input logic         rst,
     input logic         en,
     input logic         clk,
-    input logic         dout
+    output logic [4:1]  dout
 );
+
+logic [4:1]         sreg;
 
 always_ff @ (posedge clk, posedge rst)
     if (rst)
@@ -12,4 +14,5 @@ always_ff @ (posedge clk, posedge rst)
         sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
 
 assign dout = sreg;
+
 endmodule
--- task1/lsfr_tb.cpp
+++
@@ -1,4 +0,0 @@
-#include "verilated.h"
-#include "verilated_vcd_c.h"
-#include "Vlsfr.h"
-#include "vbuddy.cpp"
